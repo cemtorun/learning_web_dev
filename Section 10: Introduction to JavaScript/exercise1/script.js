b5b43e2efebbb1d7a7f3b7c97ef37e2586683a46
Cem Torun <cemtorun283@gmail.com>
2019-07-11 09:16:12 -0400

    exercise1

---
+++ Section 10: Introduction to JavaScript/exercise1/script.js
@@ -0,0 +1,7 @@
+var fName = prompt("What is your first name?");
+var lName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+console.log("Your full name is " + fName + " " + lName + ".");
+console.log("You are " + age + " years old.");
+
