4c109170543b962d0f15e5b45459a39f23e0ae0e
Cem Torun <cemtorun283@gmail.com>
2019-07-11 08:53:27 -0400

    sample js files

---
+++ Section 10: Introduction to JavaScript/script.js
@@ -0,0 +1,14 @@
+alert("Hello from JS!");
+// if you refresh index.html in browser you get this alert!
+
+// shows up like an alert
+var userName = prompt("What is your name?");
+
+// alert welcoming them
+alert("Nice to meet you " + userName + ".")
+
+// if you open console will see this message below and only there
+console.log("Welcome " + userName + ".")
+
+// console.log prints the file and the line at which the console.log occured
+// great for debugging!2
